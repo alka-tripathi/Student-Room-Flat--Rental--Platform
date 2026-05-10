dff9b93df09eb55be9531b6b540d8fe88ac52d2a
Alka Tripathi <tripathialka382@gmail.com>
2026-05-10 09:35:26 +0530

    app.js updated

--- backend/app.js
+++ backend/app.js
@@ -8,18 +8,33 @@ const cookieParser = require('cookie-parser');
 
 app.use(cookieParser());
 
-app.use(express.json());
+require('./Models/db');
 
+const AuthRouter = require('./Routes/AuthRouter');
+const RoomRouter = require('./Routes/RoomRoutes');
+
+app.set('trust proxy', 1);
+
+const { v2: cloudinary } = require('cloudinary');
+
+// ✅ Cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// ✅ Allowed Origins
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
   'https://rentalco-gwwb.onrender.com',
 ];
 
+// ✅ CORS MUST COME BEFORE ROUTES
 app.use(
   cors({
     origin: function (origin, callback) {
-      // allow requests with no origin (Postman)
       if (!origin) return callback(null, true);
 
       if (allowedOrigins.includes(origin)) {
@@ -33,19 +48,22 @@ app.use(
     credentials: true,
   }),
 );
-// Middleware
+
+// ✅ Middleware
 app.use(express.json());
 
 app.use('/uploads', express.static('uploads'));
 
-// Routes
-app.use('/auth', AuthRouter);
-app.use('/room', RoomRouter);
-
+// ✅ Test Route
 app.get('/ping', (req, res) => {
   res.send('Pong');
 });
 
+// ✅ Routes
+app.use('/auth', AuthRouter);
+app.use('/room', RoomRouter);
+
+// ✅ Server
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
